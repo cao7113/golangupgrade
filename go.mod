0ae7774ab3e49f35eb53640cc42d94db79e2fbbc
Ruijian Cao <cao7113@hotmail.com>
2019-10-22 10:33:33 +0800

    add public module call

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cao7113/golangupgrade
 
 go 1.13
 
-require github.com/cao7113/golangprivatemod v0.0.0-20191021065030-d18d0ae99f85
+require (
+	github.com/cao7113/golang v0.0.1
+	github.com/cao7113/golangprivatemod v0.0.1
+)
--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"runtime"
 
 	ptry "github.com/cao7113/golangprivatemod/try"
+
+	try "github.com/cao7113/golang/try"
 )
 
 func main() {
 	str := fmt.Sprintf("Go version: %s", runtime.Version())
 	fmt.Println(str)
 	ptry.Try()
+	try.Try()
 }
